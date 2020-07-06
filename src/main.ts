bcaa6d938665b3839355243704d62171691138a4
arihant-2310 <arihant2310@gmail.com>
2020-07-06 21:02:39 +0530

    adds swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('Save Now And Use Later')
+    .setDescription('APIS')
+    .setVersion('1.0')
+    .addTag('SaveThings')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(5009);
 }
 bootstrap();
